c4de153b72d31b297a9cd97bb8f474e76fad1992
gozaoo <gozaoo@outlook.com>
2026-06-28 22:33:50 +0800

    更新播放器组件导出结构

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,18 +1,3 @@
-/**
- * Player 组件模块
- * 
- * 提供音乐播放器相关的 Vue 组件
- * 采用解耦架构，各组件可独立使用
- */
 
-// AMLL 原生组件封装
-export { default as AMLLBackgroundRenderer } from './AMLLBackgroundRenderer.vue'
-export { default as AMLLLyricPlayer } from './AMLLLyricPlayer.vue'
-export { default as AMLLPlayerContainer } from './AMLLPlayerContainer.vue'
-
-// 原有组件
-export { default as PlayerAlbumCard } from './PlayerAlbumCard.vue'
 export { default as PlayerControlBar } from './PlayerControlBar.vue'
 export { default as PlayerControls } from './PlayerControls.vue'
-export { default as PlayerProgress } from './PlayerProgress.vue'
-export { default as PlayerVolume } from './PlayerVolume.vue'
